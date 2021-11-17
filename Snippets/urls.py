2ed0df0cd66ef922b1f466b22acb4ca421569a67
togachev <togachev@gmail.com>
2021-11-17 13:33:09 +0500

    add base.html

--- Snippets/urls.py
+++ Snippets/urls.py
@@ -14,12 +14,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 
 from MainApp import views
 
 urlpatterns = [
-    path('', views.index_page),
-    path('snippets/add', views.add_snippet_page),
-    path('snippets/list', views.snippets_page),
-]
+    path('', views.index_page, name = 'home'),
+    path('snippets/add', views.add_snippet_page, name = 'snippets_add'),
+    path('snippets/list', views.snippets_page, name = 'snippets_list'),
+]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
